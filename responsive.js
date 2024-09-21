514457d0ee34088a204e80d2b0ca45e9f4bb128a
Ra00ta <taliyaramazanli@gmail.com>
2024-09-21 14:32:41 +0400

    Deneme 19999

---
+++ responsive.js
@@ -0,0 +1,4 @@
+document.querySelector('.icon').addEventListener('click', function () {
+    const menu = document.querySelector('ul');
+    menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
+});
